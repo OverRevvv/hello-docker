68eec23197f4ed8819b057c84902d718b6fc9d26
OverRevvv <rdude1612@gmail.com>
2024-06-13 22:22:08 +0530

    docker-compose.yml

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.json({ message:'Hello Docker! 🐳'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Docker! 🐳", env: `${process.env.ENV_VAR}` });
+});
 
 app.listen(port, () => {
-  console.log(`app listening on port http://localhost:${port}`)
-})
+  console.log(`app listening on port http://localhost:${port}`);
+});
